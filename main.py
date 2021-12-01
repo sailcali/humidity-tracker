cf1678da209f269d300b733019b2b443eb652f0e
Parsec <sailcali@gmail.com>
2021-11-30 21:50:01 -0800

    cleaned up on win

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ from board import D4
 sensor = dht.DHT22(D4)
 
 while True:
-    print(sensor.humidity)
-    quit()
-    if humidity is not None and temperature is not None:
-        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
+    if sensor.humidity is not None and sensor.temperature is not None:
+        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(sensor.temperature, sensor.humidity))
     else:
         print("Failed to retrieve data from humidity sensor")
     time.sleep(5)
