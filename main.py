98b16cc0521680eccf4d1de646ee3cbff7f3d1c6
Parsec <sailcali@gmail.com>
2021-11-30 21:10:19 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import adafruit_dht as dht
+
+sensor = dht.DHT22
+DHT_PIN = 4
+while True:
+    humidity, temperature = dht.read_retry(sensor, DHT_PIN)
+    if humidity is not None and temperature is not None:
+        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
+    else:
+        print("Failed to retrieve data from humidity sensor")
