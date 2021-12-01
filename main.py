83861c2d92fb6a0b575efa1b1ed7408e03f781ca
root <root@ubuntu.lan>
2021-11-30 21:48:09 -0800

    changed on pi

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import adafruit_dht as dht
+import time
+from board import D4
+sensor = dht.DHT22(D4)
 
-sensor = dht.DHT22
-DHT_PIN = 4
 while True:
-    humidity, temperature = dht.read_retry(sensor, DHT_PIN)
+    print(sensor.humidity)
+    quit()
     if humidity is not None and temperature is not None:
         print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
     else:
         print("Failed to retrieve data from humidity sensor")
+    time.sleep(5)
